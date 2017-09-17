d6ca34be4f38d8c22c89f3123b414d6916fd2640
Vivek <Vivek@VivekKulshrestha>
2017-09-17 10:37:44 +0530

    Assignment1.2

---
+++ src/assignment/sum.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class sum {
+//Program for adding 2 variables without using the + operator
+	public static void main(String[] args) 
+	{
+		// Declaring variables
+		int a, b, sum;
+		//Assigning the values
+		a = 9;
+		b = 21;
+		sum = b-(-a);
+		System.out.println(sum);
+		
+
+	}
+
+}
